34cafe1ab3cd111f817035088bab0aac4b2dbfd7
memmm <emesenge@gmail.com>
2019-10-11 00:52:59 +0200

    added pages + test navigation

--- components/nav.js
+++ components/nav.js
@@ -1,14 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
 
-const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
-
 const Nav = () => (
   <nav>
     <ul>
@@ -17,11 +9,19 @@ const Nav = () => (
           <a>Home</a>
         </Link>
       </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <a href={href}>{label}</a>
-        </li>
-      ))}
+      
+      <li>
+      <Link href='chat'>
+        <a>Chat</a>
+        </Link>
+      </li>
+
+      <li>
+      <Link href='settings'>
+        <a>Settings</a>
+        </Link>
+      </li>
+      
     </ul>
 
     <style jsx>{`
---
+++ pages/settings.js
@@ -0,0 +1,13 @@
+export default function Settings() {
+  return (
+    <div>
+      <div>
+        <h1>Preferences</h1>
+      </div>
+      <style jsx>{`
+    
+    
+        `}</style>
+    </div>
+  );
+}
